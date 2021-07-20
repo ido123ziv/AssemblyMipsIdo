bd1ff964c1c656f2c4d54df4497645075a79fb78
idosh <idoshziv@gmail.com>
2021-07-20 23:33:12 +0300

    added mission 2, count longest word

--- mips2.asm
+++ mips2.asm
@@ -32,7 +32,7 @@ main: # main program entry
 	addi $t2, $0, 1 # $t2 will store the word count which is minimum 1
 	la $t0, StringBuffer # get StringBuffer to a worker var
 how_many_words:
-	lb $t1, $t0 # load to t1 the first character in string
+	lb $t1, ($t0) # load to t1 the first character in string
 	beq $t1, 32, add_word # if char == ' ' add a word to count
 	beq $t1, 10, finish_word # if new line end count
 	addi $t0, $t0, 1 # check next char
@@ -48,7 +48,7 @@ finish_word:
 	li $v0, 4 # print null terminated string "Number of words = "
 	syscall
 
-	la $a0, $t2 # print WordCount
+	la $a0, ($t2) # print WordCount
 	li $v0, 1 # service 1 is print integer
 	syscall
 
@@ -56,6 +56,59 @@ finish_word:
 	li $v0, 4  # print null terminated string "\n"
 	syscall
 
+# Mission 2 - Count how many letters in longest word
+# t0 -> the word t1 -> current letter t2- > odd buffer t3 -> even buffer
+	add $t2, $0, $0 # reset t2
+	la $t0, StringBuffer # get StringBuffer to a worker var
+
+t2_logger_word:  # this loop will remember odd words
+	lb $t1, ($t0) # load to t1 the first character in string
+	beq $t1, 32, next_word # if char == ' ' go to next word
+	beq $t1, 10, finish_long # if new line end count
+	addi $t2, $t2, 1 # count++ for current word
+	addi $t0, $t0, 1 # check next char
+	j t2_logger_word
+
+next_word:
+	addi $t0, $t0, 1 # check next char, continue from space
+	bgt $t2, $t3, reset_t3 # if t2 > t3 reset t2
+	add $t1, $0, $0 # reset t1 because t3 > t2
+	j t2_logger_word
+
+reset_t3:
+	add $t3, $0, $0 
+
+t3_logger_word:  # this loop will remember even words
+	lb $t1, ($t0) # load to t1 the first character in string
+	beq $t1, 32, next_word # if char == ' ' go to next word
+	beq $t1, 10, finish_long # if new line end count
+	addi $t3, $t3, 1 # count++ for current word
+	addi $t0, $t0, 1 # check next char
+	j t3_logger_word
+
+finish_long:
+	la $a0, LongestWord # load text LongestWord
+	li $v0, 4 # print null terminated string "The longest word = "
+	syscall
+
+	bgt $t2, $t3, print_odd # if t2 > t3 print odd
+	add $t6, $t3, $t0 # Store longest word, for future purposes
+	la $a0, ($t2) # print letter count
+	li $v0, 1 # service 1 is print integer
+	syscall
+	j end_task2 # goto end if printed
+
+print_odd:
+	add $t6, $t3, $t0 # Store longest word, for future purposes
+	la $a0, ($t2) # print letter count
+	li $v0, 1 # service 1 is print integer
+	syscall
+
+# echo finish
+end_task2:
+	la $a0, NewLine # load NewLine
+	li $v0, 4  # print null terminated string "\n"
+	syscall
 
 	#ADD data valiadation
 	li $v0, 10 # Exit 
